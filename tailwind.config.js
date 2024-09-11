a3213ed41db4b5be9b47b9e495761b3841ef4edf
ranyeh24 <real.ranyeh24@gmail.com>
2024-09-11 16:34:26 +0700

    [add] init tailwind css config

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  prefix: "tw-",
+  darkMode: ["selector", "[data-theme-mode=dark"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
